79cd6a91646cd516aba4785d2e253c332485acb8
Pame Arnez <wen.pame04@gmail.com>
2019-01-24 19:09:36 -0400

    slider with logos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SwiperComponent, SwiperModule } from 'ngx-useful-swiper';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demoSwiper';
+  @ViewChild('usefulSwiper') usefulSwiper: SwiperComponent;
+
+  // config: any = {
+  //   autoplay: 3000, // Autoplay option having value in milliseconds
+  //   initialSlide: 0, // Slide Index Starting from 0
+  //   slidesPerView: 1, // Slides Visible in Single View Default is 1
+  //   pagination: '.swiper-pagination', // Pagination Class defined
+  //   paginationClickable: true, // Making pagination dots clicable
+  //   nextButton: '.swiper-button-next', // Class for next button
+  //   prevButton: '.swiper-button-prev', // Class for prev button
+  //   spaceBetween: 30, // Space between each Item
+  //   dynamicBullets: true,
+  // };
+
+  config: any = {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    paginationClickable: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: 10
+  };
+
+  listBusiness = [
+    {name: "image1"},
+    {name: "image2"},
+    {name: "image3"},
+    {name: "image4"},
+    {name: "image1"},
+    {name: "image2"},
+    {name: "image2"},
+    {name: "image3"},
+    {name: "image4"}
+  ]
+
+
+
+  next() {
+    this.usefulSwiper.swiper.slideNext();
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SwiperModule } from 'ngx-useful-swiper';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SwiperModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
